446430dec2b7224c784ddb256f538c6f3f664784
AbhiShirke1 <91108875+AbhiShirke1@users.noreply.github.com>
2022-11-12 13:02:43 +0530

    some commit

---
+++ models/caretaker.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const caretakerSchema = new mongoose.Schema({
+    email:{
+        type: String,
+    },
+
+    fname:{
+        type: String,
+    },
+
+    lname:{
+        type: String,
+    },
+
+    gender:{
+        type: Number,
+    },
+
+    age:{
+        type: Number,
+    },
+
+    experience:{
+        type: Number,
+    },
+
+    charge:{
+        type: String
+    },
+
+    slot: [{
+        type: String
+    }],
+
+    password:{
+        type: String
+    },
+
+    usernames:[{
+        type: String
+    }],
+
+    useremail:[{
+        type: String
+    }]
+
+})
+
+const Caretaker = new mongoose.model("Caretaker", caretakerSchema);
+module.exports = Caretaker;
+
